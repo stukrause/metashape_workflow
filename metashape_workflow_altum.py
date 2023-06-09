dc3bd43a4bcae1bf648fb31a00c0f54fe2cfc45b
stukrause <56559328+stukrause@users.noreply.github.com>
2023-06-09 13:02:01 -0600

    Update metashape_workflow_altum.py

--- metashape_workflow_altum.py
+++ metashape_workflow_altum.py
@@ -83,7 +83,7 @@ for i in range (0,len(foldernames)):
 
     crs = Metashape.CoordinateSystem(wkt)
     #crs = Metashape.CoordinateSystem("EPSG::25833")
-    shape_path = "D:/process/britz/utilities/britz_boundary_buffer3m.shp"
+    shape_path = "D:/path/to/shapefile.shp"
     chunk.importShapes(path=shape_path, crs=crs, boundary_type=Metashape.Shape.OuterBoundary)
     #shape.boundary_type = Metashape.Shape.BoundaryType.OuterBoundary
 
